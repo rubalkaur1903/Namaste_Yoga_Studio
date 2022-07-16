4fbee7dd4ca4b167ba8c997673849bcbb7596e66
Rubal Kaur <84761627+rubalkaur1903@users.noreply.github.com>
2022-07-15 18:52:22 -0700

    Added and Styled the Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { FaInstagramSquare } from 'react-icons/fa';
+import { BsTwitter } from 'react-icons/bs';
+
+import '../cssFiles/footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <h1></h1>
+        <div className="footer-container">
+            <div className="footer-heading">
+                <p>Follow us on Instagram and Twitter to know more about our events and workshops!</p>
+            </div>
+            <div className="socials-container">
+                <a className="social-media-instagram"><FaInstagramSquare /></a>
+                <a className="social-media-twitter"><BsTwitter /></a>
+            </div>
         </div>
     )
 }
